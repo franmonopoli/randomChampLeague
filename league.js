cc2881ca57c8b1d6dc083befb86eabfec3a4d08f
Fran <clear>
2023-01-29 23:55:23 -0300

    Segundo Commit

--- league.js
+++ league.js
@@ -1,6 +1,9 @@
 'use strict'
 let div_id = document.getElementById('champ')
 let image_id = document.getElementById('champPic')
+let opgg_url = 'https://www.op.gg/champions/'
+
+
 
 let json_url = './es_AR/champion.json'
 fetch(json_url)
@@ -10,31 +13,35 @@ fetch(json_url)
         return champion_data(champions.data)
     })
     .then(all_champs_id => {
-        let random_int = getRandomInt(0,161)
-        let champName = all_champs_id[random_int]
-        let imageUrl = '../img/champion/tiles/'+champName+'_0.jpg' //Asi puedo cambiar la imagen del champion
-        maquetado(champName, imageUrl)
+        let getChampButton = document.getElementById('button')
+        let randomChamp = getRandomChamp(all_champs_id)
+        getChampButton.addEventListener('click', ()=>{
+             maquetado(randomChamp)
+        })
         
+        let getChampRunes = document.getElementById('redirect')
+        getChampRunes.addEventListener('click', ()=>{
+            let champNameLowerCase = randomChamp.toLowerCase()
+            window.open(opgg_url+champNameLowerCase)
+        }) 
     })
 
 
+function getRandomChamp(champs_id){
+    let random_int = getRandomInt(0,161)
+    let champName = champs_id[random_int]
+    return champName
+}
+
 function champion_data(data){
     let str_champs = ''
-
     for( let element in data){
         str_champs = str_champs+element+','; //Esto lo hago para poder tener todos juntos separados por una coma
     }
-
     let all_champs = str_champs.split(',')
     return all_champs
 }
 
-function randomChamp(data_list){
-    console.log(data_list)
-    
-    console.log(random_int)
-    return 
-}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -42,18 +49,15 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function maquetado(champName, image){
+function maquetado(champName){
+    let image = '../img/champion/tiles/'+champName+'_0.jpg' //Asi puedo cambiar la imagen del champion
     document.getElementById('champName').innerHTML = champName
-    let buttonPressed = document.getElementById('button')
-    buttonPressed.addEventListener('click', ()=>{
-        div_id.style.display = 'block'
-        image_id.src = image
-        buttonPressed.disabled = true
-})
+    image_id.src = image
+    div_id.style.display = 'block'
+}
 
 function refreshPage(){
     let reload_button = document.getElementById('reload')
     reload_button.addEventListener('click', location.reload())
 }
 
-}
