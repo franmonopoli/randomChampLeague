a1237a7f5916af348d01ef0a5708444355af87c9
Fran <clear>
2023-01-31 19:36:23 -0300

    En total funcionamiento tanto el campeon aleatorio y el obtener runas, falta la parte de lineas y terminar con el tema de la visualizacion

--- league.js
+++ league.js
@@ -11,27 +11,21 @@ let championsData = await fetch(json_url) // Termina esto y devuelve la lista de
     .then(response => response.json())
     .then(champions => champion_data(champions.data))
     
-
-championsData[0] = 'Aatrox' // No se porque el primero dedvuelve "undefinedAatrox"
-let randomChamp = getRandomChamp(championsData)
-console.log(championsData)
+let randomChamp
 
 /* Boton para obtener campeon random */
 let getChampButton = document.getElementById('randomChamp')
-getChampButton.addEventListener('click', event =>{
-    let image = '../img/champion/tiles/'+ randomChamp +'_0.jpg' //Asi puedo cambiar la imagen del champion
-    document.getElementById('champName').innerHTML = randomChamp
+getChampButton.addEventListener('click', e =>{
+    console.log('click')
+    let champName = getRandomChamp(championsData)
+    let image = '../img/champion/tiles/'+ champName +'_0.jpg' //Asi puedo cambiar la imagen del champion
+    document.getElementById('champName').innerHTML = champName
     image_id.src = image
     div_id.style.display = 'block'
+    randomChamp = champName
 
+    redirect(champName) // Cambia el HREF del a para que dirija por imagen a la direccion url
 })
-
-/* Boton para obtener las runas campeon */
-let getChampRunes = document.getElementById('redirect')
-getChampRunes.addEventListener('click', event =>{
-    let champNameLowerCase = randomChamp.toLowerCase()
-    window.open(opgg_url+champNameLowerCase)
-}) 
 
 /* Boton para obtener dos lineas aleatorias */
 const lines = ['Top', 'Jungla', 'Mid', 'Adc', 'Soporte', 'Autofill']
@@ -39,9 +33,6 @@ let getRandomLines = document.getElementById('selectLines')
 getRandomLines.addEventListener('click', e =>{
     let randomLines = getTwoRandomLines(lines)
 })
-
-
-
 
 
 /* Funciones  */
@@ -52,14 +43,8 @@ function getRandomChamp(champs_id){
 }
 
 function champion_data(data){
-    let str_champs;
-    for( let element in data){
-        str_champs = str_champs+element+','; //Esto lo hago para poder tener todos juntos separados por una coma
-    }
-    let all_champs = str_champs.split(',')
-    return all_champs
+    return Object.keys(data) // Devuevle todas las keys 
 }
-
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -78,4 +63,10 @@ function getTwoRandomLines(array_lines){
 
     let secondRandomNumber 
 
+}
+
+function redirect(champName){
+    let changeHref = document.getElementById('redirectOpgg')
+    let champLowerCase = String(champName).toLowerCase()
+    changeHref.href = opgg_url+champLowerCase
 }
